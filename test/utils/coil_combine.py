9e91d3eb0fafb81e327a895671cac7c9546097ae
Mehrdad Anvari-Fard <mehrdad.anvarifard@gmail.com>
2025-08-16 09:36:25 +0330

    add some utilites for testing

---
+++ test/utils/coil_combine.py
@@ -0,0 +1,23 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+import torch
+
+def rss(data: torch.Tensor, dim: int = 0) -> torch.Tensor:
+    """
+    Compute the Root Sum of Squares (RSS).
+
+    RSS is computed assuming that dim is the coil dimension.
+
+    Args:
+        data: The input tensor
+        dim: The dimensions along which to apply the RSS transform
+
+    Returns:
+        The RSS value.
+    """
+    return torch.sqrt((data**2).sum(dim))
