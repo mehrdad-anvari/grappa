a8c3d24594a2d4aa2dd49ae5aa0878522a57406f
Mehrdad Anvari-Fard <mehrdad.anvarifard@gmail.com>
2025-08-17 12:21:33 +0330

    use create_mask function

--- test/test_mask.py
+++ test/test_mask.py
@@ -3,10 +3,13 @@ from utils.transforms import to_tensor, complex_center_crop
 from utils.fftc import ifft2c_new
 from utils.math import complex_abs
 from utils.coil_combine import rss
-from utils.subsample import MagicMaskFractionFunc
+from utils.subsample import create_mask_for_mask_type
+import torch
 import matplotlib.pyplot as plt
 
-maskClass = MagicMaskFractionFunc(center_fractions=[0.08], accelerations=[4])
+maskClass = create_mask_for_mask_type(
+    mask_type_str="one_sided", center_fractions=[0.08], accelerations=[4]
+)
 
 
 def transform(kspace, mask, target, attrs, name, dataslice):
@@ -33,6 +36,13 @@ for image, mask in dataset:
     plt.figure(figsize=(16, 8))
     plt.plot(mask.flatten(), marker=".", linestyle="None")
     plt.savefig("mask.png")
+    acquired_cols = int(torch.sum(mask.flatten()).item())
+    all_cols = mask.flatten().shape[0]
+    print(
+        acquired_cols,
+        all_cols,
+        f"{acquired_cols / all_cols:.4f}",
+    )
     plt.axis(False)
     plt.imshow(image, cmap="gray")
     plt.savefig("test2.png")
